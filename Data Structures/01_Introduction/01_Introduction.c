6162056a188eb880b56e0dc25a24e175e3662c3c
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2023-11-04 16:54:00 +0500

    Introduction to Data Structures.

---
+++ Data Structures/01_Introduction/01_Introduction.c
@@ -0,0 +1,69 @@
+# include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+
+    printf("Introduction of Data Structures.\n");
+
+    // A good knowledge of Data Structures is a must.
+
+    // In order ot design good software systems, we need to have a very deep knowledge of Data Structures.
+
+    // We deal with data all the time.
+    // Data Structures matters.
+
+    // Let's pick up some examples form our day to day life where organizing data helps us.
+        // For Example:
+            // A Dictionary - Where a large number of words (data) is written, But how we find our word very fast?
+            // Because they are sorted.
+            // What if the words are not sorted?
+            // Then it would be very hard to find a word among so many words quickly.
+            // So, the dictionary is organized kind of like sorted list of words.
+            // City Map - where all the data of streets, lakes, roads is organized in the form of geometry and shapes.
+            // We show the map data in the form of geometry.
+            // So map data has to be structured like this.
+            // Cashbook - where all the data like date, debit, credit, total each and everything is written.
+            // It'll make more sense to organize this type of data in the form of Table.
+        // So, different kind of structures are needed for organizing different kind of data.
+    
+    // Computer works with all kind of data.
+        // Computer works with text, images, videos, etc...
+        // These are all data.
+        // How we store, organize data in computer matters.
+        // Because computer deals with really really large data.
+        // It will take more power and energy and time to do some operations on data if we don't store the data the optimal way possible.
+
+    // Definition of Data Structures.
+        // A data structure is a way to store and organize data in a computer, so that it can be used efficiently.
+
+    // We talk about Data Structures as:
+        // 1. Mathematical / Logical models or Abstract Data Types.
+            // When we talk Data Structures in Mathematical / Logical models or Abstract Data Types, wej just look at the abstract look of them.
+            // We just look them as high level.
+            // For Example:
+                // A TV - We just see TV as a electric device which turns on and off, we can watch TV.
+                // We don't bother to look at understanding how the TV is made up of. How it is working. Stuff like that.
+            // For Example:
+                // A list:
+                    // If we take a look at list by higher level.
+                    // List is a abstract data type.
+                    // Which store a given number of elements of anytype.
+                    // Which read element by position.
+                    // Which modify element at a position.
+            // Basically most of the programming languages have the basic Abstract Data Types setup by default. We just need to use them.
+            // For Example:
+                // Arrays, etc...
+        // 2. Implementation.
+            // Implementation is we can understand how really things works?
+            // We can implement our own Abstract Data Types using the same language. And we can use our own ADTs as well.
+            // For Example:
+                // In C/C++, we can implement this list ADT to work with.
+                // We can implement Linked List.. etc...
+
+        // Abstract Data Types.
+        // Abstract Data Types define data and operations, but no implementations.
+
+        // Implementation.
+        // Implementation is basically implementing and understand things in low level, how this specif operation happen? etc...
+
+    return 0;
+}
