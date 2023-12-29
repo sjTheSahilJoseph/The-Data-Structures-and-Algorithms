7a6b1527efee50223951184e0b86e33eaa401d2c
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2023-12-29 20:19:11 +0500

    01

---
+++ Data Structures and Algorithms/04_What_is_an_Algorithm.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(int argc, char const* argv[]) {
+    printf("What is an Algorithm?\n");
+
+    /*
+
+        What is an Algorithm?
+        A process or set of rules to be followed in calculations or other
+       problem solving operations, especially by a computer. An algorithm is a
+       finite set of instructures that are carried in a specific order to
+       perform specific task.
+
+        Algorithms typically have the following characteristics:
+            1. Inputs : 0 or more input values.
+            2. Outputs : 1 or more than 1 output.
+            3. Unambiguity : Clear and Simple instructions.
+            4. Finiteness : Limited number of instructions.
+            5. Effectiveness : Each instruction has an impact on the overall
+       process.
+
+        Real World Example of an Algorithm.
+        Algorithm (also known as process) to make a lemonade.
+        1. Cut your lemon in half.
+        2. Squeeze all the juice out of it that you can.
+        3. Pour your juice into a container with 1/4 cup sugar.
+        4. Add a very small amount of water to your container.
+        5. Stir your solution until sugar dissolves.
+        6. Fill up container with water and add ice.
+        7. Put your lemonade in the fridge for five minutes.
+        8. Serve and enjoy!
+
+        Example of an Algorithm in Programming.
+        Write an algorithm to add two numbers entered by user.
+        1. Start
+        2. Declare Variables num1, num2, and sum.
+        3. Read values num1 and num2.
+        4. Add num1 and num2 and assign the result to sum. (sum <- num1 + num2).
+        5. Display sum.
+        6. Stop.
+
+    */
+
+    return 0;
+}
